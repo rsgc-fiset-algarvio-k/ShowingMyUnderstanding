dd8a629130db6f5fd42dc25b4b8023d8af3010cf
Student <student@macbook-pro-fiset-algarvio.local>
2017-02-25 12:34:05 -0500

    Made a shirt design

--- ShowingMyUnderstanding.playground/Pages/Sequence - Create an Example.xcplaygroundpage/Contents.swift
+++ ShowingMyUnderstanding.playground/Pages/Sequence - Create an Example.xcplaygroundpage/Contents.swift
@@ -23,7 +23,38 @@ import PlaygroundSupport
 // Create canvas
 let canvas = Canvas(width: 300, height: 300)
 
-// Replace this comment and add your code below...
+    var a = random(from: 0, toButNotIncluding: 361)
+    var b = random(from: 0, toButNotIncluding: 361)
+    var c = random(from: 0, toButNotIncluding: 361)
+    var d = random(from: 0, toButNotIncluding: 361)
+    var e = random(from: 0, toButNotIncluding: 361)
+    var f = random(from: 0, toButNotIncluding: 361)
+    var g = random(from: 0, toButNotIncluding: 361)
+    var h = random(from: 0, toButNotIncluding: 361)
+    var i = random(from: 0, toButNotIncluding: 361)
+
+for x in stride(from: 0, through: 100, by: 10) {
+    canvas.drawShapesWithBorders = true
+    canvas.drawShapesWithFill = false
+    canvas.borderColor = Color(hue: a, saturation: 100, brightness: 100, alpha: 100)
+    canvas.drawEllipse(centreX: 150, centreY: 150, width: x, height: x)
+    canvas.borderColor = Color(hue: b, saturation: 100, brightness: 100, alpha: 100)
+    canvas.drawEllipse(centreX: x / 2 + 200, centreY: 150, width: x, height: x)
+    canvas.borderColor = Color(hue: c, saturation: 100, brightness: 100, alpha: 100)
+    canvas.drawEllipse(centreX: 100 - (x / 2), centreY: 150, width: x, height: x)
+    canvas.borderColor = Color(hue: d, saturation: 100, brightness: 100, alpha: 100)
+    canvas.drawEllipse(centreX: 150, centreY: 100 - x / 2, width: x, height: x)
+    canvas.borderColor = Color(hue: e, saturation: 100, brightness: 100, alpha: 100)
+    canvas.drawEllipse(centreX: 150, centreY: x / 2 + 200, width: x, height: x)
+    canvas.borderColor = Color(hue: f, saturation: 100, brightness: 100, alpha: 100)
+    canvas.drawEllipse(centreX: x / 2, centreY: x / 2, width: x, height: x)
+    canvas.borderColor = Color(hue: g, saturation: 100, brightness: 100, alpha: 100)
+    canvas.drawEllipse(centreX: 300 - x / 2, centreY: x / 2, width: x, height: x)
+    canvas.borderColor = Color(hue: h, saturation: 100, brightness: 100, alpha: 100)
+    canvas.drawEllipse(centreX: x / 2, centreY: 300 - x / 2, width: x, height: x)
+    canvas.borderColor = Color(hue: i, saturation: 100, brightness: 100, alpha: 100)
+    canvas.drawEllipse(centreX: 300 - x / 2, centreY: 300 - x / 2, width: x, height: x)
+}
 
 /*:
  ## Template code
