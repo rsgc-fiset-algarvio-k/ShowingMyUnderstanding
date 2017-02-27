305cedb610f963bd40894e56188c3e29e4202bca
Student <student@macbook-pro-fiset-algarvio.local>
2017-02-27 15:25:22 -0500

    Made a cool design with circles only.

--- ShowingMyUnderstanding.playground/Pages/Iteration - Create an Example.xcplaygroundpage/Contents.swift
+++ ShowingMyUnderstanding.playground/Pages/Iteration - Create an Example.xcplaygroundpage/Contents.swift
@@ -19,23 +19,21 @@ import PlaygroundSupport
 // Create canvas
 let canvas = Canvas(width: 300, height: 300)
 
-// Random Colors
+// random Color
 var a = random(from: 0, toButNotIncluding: 361)
 
-// Cones
-for x in stride(from: 0, through: 100, by: 10) {
-    canvas.drawShapesWithBorders = true
-    canvas.drawShapesWithFill = false
-    canvas.borderColor = Color(hue: a + x, saturation: 100, brightness: 100, alpha: 100)
-    canvas.drawEllipse(centreX: x, centreY: x, width: x, height: x)
-    canvas.drawEllipse(centreX: 300 - x, centreY: 300 - x, width: x, height: x)
-    canvas.drawEllipse(centreX: 300 - x, centreY: x, width: x, height: x)
-    canvas.drawEllipse(centreX: x, centreY: 300 - x, width: x, height: x)
-    canvas.drawEllipse(centreX: 150, centreY: x, width: x, height: x)
-    canvas.drawEllipse(centreX: x, centreY: 150, width: x, height: x)
-    canvas.drawEllipse(centreX: 150, centreY: 300 - x, width: x, height: x)
-    canvas.drawEllipse(centreX: 300 - x, centreY: 150, width: x, height: x)
-    canvas.drawEllipse(centreX: 150, centreY: 150, width: x * 4, height: x * 4)
+for z in stride(from: 0, through: 100, by: 10) {
+    for x in stride(from: 0, through: 200, by: 100) {
+        for y in stride(from: 0, through: 200, by: 100) {
+            canvas.drawShapesWithBorders = true
+            canvas.drawShapesWithFill = false
+            canvas.borderColor = Color(hue: a + z, saturation: 100, brightness: 100, alpha: 100)
+            canvas.drawEllipse(centreX: z / 2 + x, centreY: z / 2 + y, width: z, height: z)
+            canvas.drawEllipse(centreX: 100 - z / 2 + x, centreY: 100 - z / 2 + y, width: z, height: z)
+            canvas.drawEllipse(centreX: 100 - z / 2 + x, centreY: z / 2 + y, width: z, height: z)
+            canvas.drawEllipse(centreX: z / 2 + x, centreY: 100 - z / 2 + y, width: z, height: z)
+        }
+    }
 }
 
 /*:
--- ShowingMyUnderstanding.playground/Pages/Sequence - Create an Example.xcplaygroundpage/Contents.swift
+++ ShowingMyUnderstanding.playground/Pages/Sequence - Create an Example.xcplaygroundpage/Contents.swift
@@ -23,7 +23,23 @@ import PlaygroundSupport
 // Create canvas
 let canvas = Canvas(width: 300, height: 300)
 
+// random Color
+var a = random(from: 0, toButNotIncluding: 361)
 
+// The Circle-squares
+for z in stride(from: 0, through: 100, by: 10) {
+    for x in stride(from: 0, through: 200, by: 100) {
+        for y in stride(from: 0, through: 200, by: 100) {
+    canvas.drawShapesWithBorders = true
+    canvas.drawShapesWithFill = false
+    canvas.borderColor = Color(hue: a * z, saturation: 100, brightness: 100, alpha: 100)
+    canvas.drawEllipse(centreX: z / 2 + x, centreY: z / 2 + y, width: z, height: z)
+    canvas.drawEllipse(centreX: 100 - z / 2 + x, centreY: 100 - z / 2 + y, width: z, height: z)
+    canvas.drawEllipse(centreX: 100 - z / 2 + x, centreY: z / 2 + y, width: z, height: z)
+    canvas.drawEllipse(centreX: z / 2 + x, centreY: 100 - z / 2 + y, width: z, height: z)
+        }
+    }
+}
 /*:
  ## Template code
  The code below is necessary to see results in the Assistant Editor at right. Please do not remove.
