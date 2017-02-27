10f498367d1b179f46ff6253fd9e74b4fd6f64f9
Student <student@macbook-pro-fiset-algarvio.local>
2017-02-27 12:41:18 -0500

    Made a cool cones and colors design

--- ShowingMyUnderstanding.playground/Pages/Iteration - Create an Example.xcplaygroundpage/Contents.swift
+++ ShowingMyUnderstanding.playground/Pages/Iteration - Create an Example.xcplaygroundpage/Contents.swift
@@ -19,7 +19,24 @@ import PlaygroundSupport
 // Create canvas
 let canvas = Canvas(width: 300, height: 300)
 
-// Replace this comment and add your code below...
+// Random Colors
+var a = random(from: 0, toButNotIncluding: 361)
+
+// Cones
+for x in stride(from: 0, through: 100, by: 10) {
+    canvas.drawShapesWithBorders = true
+    canvas.drawShapesWithFill = false
+    canvas.borderColor = Color(hue: a + x, saturation: 100, brightness: 100, alpha: 100)
+    canvas.drawEllipse(centreX: x, centreY: x, width: x, height: x)
+    canvas.drawEllipse(centreX: 300 - x, centreY: 300 - x, width: x, height: x)
+    canvas.drawEllipse(centreX: 300 - x, centreY: x, width: x, height: x)
+    canvas.drawEllipse(centreX: x, centreY: 300 - x, width: x, height: x)
+    canvas.drawEllipse(centreX: 150, centreY: x, width: x, height: x)
+    canvas.drawEllipse(centreX: x, centreY: 150, width: x, height: x)
+    canvas.drawEllipse(centreX: 150, centreY: 300 - x, width: x, height: x)
+    canvas.drawEllipse(centreX: 300 - x, centreY: 150, width: x, height: x)
+    canvas.drawEllipse(centreX: 150, centreY: 150, width: x * 4, height: x * 4)
+}
 
 /*:
  ## Template code
--- ShowingMyUnderstanding.playground/Pages/Sequence - Create an Example.xcplaygroundpage/Contents.swift
+++ ShowingMyUnderstanding.playground/Pages/Sequence - Create an Example.xcplaygroundpage/Contents.swift
@@ -23,49 +23,7 @@ import PlaygroundSupport
 // Create canvas
 let canvas = Canvas(width: 300, height: 300)
 
-// The random variables
-    var a = random(from: 0, toButNotIncluding: 361)
-    var b = random(from: 0, toButNotIncluding: 361)
-    var c = random(from: 0, toButNotIncluding: 361)
-    var d = random(from: 0, toButNotIncluding: 361)
-    var e = random(from: 0, toButNotIncluding: 361)
-    var f = random(from: 0, toButNotIncluding: 361)
-    var g = random(from: 0, toButNotIncluding: 361)
-    var h = random(from: 0, toButNotIncluding: 361)
-    var i = random(from: 0, toButNotIncluding: 361)
 
-// The Circles
-for x in stride(from: 0, through: 100, by: 10) {
-    canvas.drawShapesWithBorders = true
-    canvas.drawShapesWithFill = false
-// Center
-    canvas.borderColor = Color(hue: a * x, saturation: 100, brightness: 100, alpha: 100)
-    canvas.drawEllipse(centreX: 150, centreY: 150, width: x, height: x)
-// Middle Right
-    canvas.borderColor = Color(hue: b * x, saturation: 100, brightness: 100, alpha: 100)
-    canvas.drawEllipse(centreX: x / 2 + 200, centreY: 150, width: x, height: x)
-// Middle Left
-    canvas.borderColor = Color(hue: c * x, saturation: 100, brightness: 100, alpha: 100)
-    canvas.drawEllipse(centreX: 100 - (x / 2), centreY: 150, width: x, height: x)
-// Bottom Middle
-    canvas.borderColor = Color(hue: d * x, saturation: 100, brightness: 100, alpha: 100)
-    canvas.drawEllipse(centreX: 150, centreY: 100 - x / 2, width: x, height: x)
-// Top Middle
-    canvas.borderColor = Color(hue: e * x, saturation: 100, brightness: 100, alpha: 100)
-    canvas.drawEllipse(centreX: 150, centreY: x / 2 + 200, width: x, height: x)
-// Bottom Left
-    canvas.borderColor = Color(hue: f * x, saturation: 100, brightness: 100, alpha: 100)
-    canvas.drawEllipse(centreX: x / 2, centreY: x / 2, width: x, height: x)
-// Bottom Right
-    canvas.borderColor = Color(hue: g * x, saturation: 100, brightness: 100, alpha: 100)
-    canvas.drawEllipse(centreX: 300 - x / 2, centreY: x / 2, width: x, height: x)
-// Top Left
-    canvas.borderColor = Color(hue: h * x, saturation: 100, brightness: 100, alpha: 100)
-    canvas.drawEllipse(centreX: x / 2, centreY: 300 - x / 2, width: x, height: x)
-// Top Right
-    canvas.borderColor = Color(hue: i * x, saturation: 100, brightness: 100, alpha: 100)
-    canvas.drawEllipse(centreX: 300 - x / 2, centreY: 300 - x / 2, width: x, height: x)
-}
 /*:
  ## Template code
  The code below is necessary to see results in the Assistant Editor at right. Please do not remove.
